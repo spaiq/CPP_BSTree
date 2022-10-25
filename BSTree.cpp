bfcf7cf04b71377cd183340a9c70fa8d8f1199e8
spaiq <112722476+spaiq@users.noreply.github.com>
2022-10-25 23:17:43 +0200

    feat: created a node structure

--- BSTree.cpp
+++ BSTree.cpp
@@ -2,6 +2,27 @@
 
 template<typename T>
 class BST {
+public:
+    struct Node {
+        T data;
+        Node* parent;
+        Node* left;
+        Node* right;
+
+        Node(const T& data, Node* parent = nullptr, Node* left = nullptr, Node* right = nullptr)
+            : data(data), parent(parent), left(left),  right(right) {
+        }
+
+        Node(T&& data, Node* parent = nullptr, Node* left = nullptr, Node* right = nullptr)
+            : data(data), parent(parent), left(left), right(right) {
+        }
+
+        ~Node() {
+            parent = nullptr;
+            left = nullptr;
+            right = nullptr;
+        }
+    };
 };
 
 int main()
